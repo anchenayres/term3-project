3996a5106f9dd3fdfa161f9a2cf69916a887e9b8
anchenayres <91013859+anchenayres@users.noreply.github.com>
2022-08-02 00:25:42 +0200

    mockup styling almost completed
    for the individual product pages

--- client/src/Components/Individual.js
+++ client/src/Components/Individual.js
@@ -12,9 +12,53 @@ const Individual = () => {
         <div className="products"><a href="/Products">Products</a></div>
         <div className="login"><a href="/">Sign In</a></div>
         <div className="cart"><a href="/Cart">Cart</a></div>
-
-
     </div>
+
+    <div className="welcome-image"></div>
+
+    <div className="img1"></div>
+    <div className="img2"></div>
+    <div className="img3"></div>
+    
+    <div className="product-des"></div>
+    <div className="size-chart"></div>
+
+
+    <select className="fabric-drop" name="selecting">
+        <option>Select a Fabric</option>
+        <option>Cotton</option>
+        <option>Leather</option>
+        <option>Nylon</option>
+        <option>Metal</option>
+    </select>
+
+    <select className="colour-drop" name="selecting">
+        <option>Select a Colour</option>
+        <option>Brown</option>
+        <option>Black</option>
+        <option>Blue</option>
+        <option>Pink</option>
+    </select>
+
+    <select className="size-drop" name="selecting">
+        <option>Select a Size</option>
+        <option>XS</option>
+        <option>S</option>
+        <option>M</option>
+        <option>L</option>
+        <option>XL</option>
+    </select>
+
+    <select className="quantity-drop" name="selecting">
+        <option>Select a Quantity</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+    </select>
+
+
+    <div className="footer"></div>
 
     </>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './login.css';
+import './individual.css';
+
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
