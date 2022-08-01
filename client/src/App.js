7b05fc9e861a348a520f822630a901e6922fc6ad
anchenayres <91013859+anchenayres@users.noreply.github.com>
2022-08-01 23:06:42 +0200

    Page links
    most of the page links are set up from the home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./Components/Navigation";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
 import Products from "./Components/Products";
+import Individual from "./Components/Individual";
 import { Routes, Route } from "react-router-dom";
 
 
@@ -15,7 +16,7 @@ function App() {
         <Route path="/" element = {<Login />} />
         <Route path="/Home" element = {<Home />} />
         <Route path="/Products" element = {<Products />} />
-
+        <Route path="/Individual" element = {<Individual />} />
       </Routes>
     </div>
     </>
---
+++ client/src/Components/Individual.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Individual = () => {
+
+    return (
+    <>
+
+    <div className="nav">
+        <div className="logo"></div>
+        <div className="home"><a href="/Home">Home</a></div>
+        <div className="products"><a href="/Products">Products</a></div>
+        <div className="login"><a href="/">Sign In</a></div>
+        <div className="cart"><a href="/Cart">Cart</a></div>
+
+
+    </div>
+
+    </>
+    )
+};
+
+export default Individual;
