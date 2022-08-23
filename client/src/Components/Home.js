a609fb22bd566b2c3912dc1aac418df01689e316
anchenayres <91013859+anchenayres@users.noreply.github.com>
2022-08-23 09:18:35 +0200

    testing
    testing the slider in the home page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,6 +4,26 @@ import React from "react";
 
 const Home = () => {
 
+    const buttons = document.querySelectorAll("[data-carousel-button]")
+
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+          .closest("[data-carousel]")
+          .querySelector("[data-slides]")
+    
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+    
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+      })
+    })
+
+
     return (
     <>
 
@@ -15,11 +35,29 @@ const Home = () => {
         <div className="cart"><a href="/Cart">Cart</a></div>
     </div>
 
-    <div className="welcome-image">
+    {/* <div className="welcome-image">
         <div class="slide1"></div>
         <div class="slide2"></div>
         <div class="slide3"></div>
+    </div> */}
+ <section aria-label="Newest Photos">
+    <div className="carousel" data-carousel>
+      <button className="carousel-button prev" data-carousel-button="prev">&#8656;</button>
+      <button className="carousel-button next" data-carousel-button="next">&#8658;</button>
+      <ul data-slides>
+        <li className="slide" data-active>
+          <img src="https://source.unsplash.com/78A265wPiO4" alt="Nature Image #1">
+        </img></li>
+        <li className="slide">
+          <img src="https://source.unsplash.com/eOpewngf68w" alt="Nature Image #2">
+        </img></li>
+        <li className="slide">
+          <img src="https://source.unsplash.com/ndN00KmbJ1c" alt="Nature Image #3">
+        </img></li>
+      </ul>
     </div>
+  </section>
+    
 
 
 
