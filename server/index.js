8146674c4ba7acb21c6f42d289d6e518904bf1d3
anchenayres <21100284@virtualwindow.co.za>
2022-10-06 13:26:46 +0200

    MongoDB
    Setting up MongoDB from the beginning

--- server/index.js
+++ server/index.js
@@ -3,29 +3,37 @@ const products = require('./products')
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+
 require('dotenv/config');
 
 const app = express();
-
-// get all products
-app.get('/api/products', (req, res) => {
-    res.send(products);
-});
-
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended: true}));
 
 app.use(routes);
 
-
-mongoose.connect(process.env.DB_CONNECTION, () => {
-    console.log("Mongo DB Atlas Connection Established");
+mongoose.connect(process.env.DB_CONNECTION, (err) => {
+    if(err){
+        console.log("No Mongo DB Atlas Connection Established" + err);
+    } else {
+        console.log("Mongo DB Atlas Connection Established");
+    }
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{console.log(`Server started on Port: ${PORT}`)});
+
+
+// get all products
+// app.get('/api/products', (req, res) => {
+//     res.send(products);
+// });
+
+
+
+
