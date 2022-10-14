c826edd4be456af90500a53a6d4c3a9cd517e310
anchenayres <21100284@virtualwindow.co.za>
2022-10-14 08:05:02 +0200

    index
    Index.js file missing

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './login.css';
-import './individual.css';
-import './individual.css';
-import './cart.css';
-import './checkout.css';
-import './products.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
--- server/models/addProduct.js
+++ server/models/addProduct.js
@@ -1,4 +1,17 @@
 const mongoose = require('mongoose');
 
+const newProduct = new newProductsModel ({
+    name: req.body.name,
+    stock: req.body.stock,
+    price: req.body.price,
+    availStock: {
+        size: req.body.availStock.size,
+        quantity: req.availStock.body.quantity,
+        material: req.body.availStock.material,
+        colour: req.body.availStock.colour
+    },
+    description: req.body.description
+})
 
-module.exports = mongoose.model('product', addProduct); // ADD products orders users (no schema in users)
+
+module.exports = mongoose.model('product', addProduct); 
